a528e9a2c4cbf39fad1e3782d18916d551011ac9
Tabas32 <marian.sabat@centrum.sk>
2022-10-23 12:59:09 +0200

    Change master to main

---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/src/gameCore/Board.cpp" "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/build/CMakeFiles/game.dir/src/gameCore/Board.cpp.o"
+  "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/src/gameCore/Chess_game.cpp" "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/build/CMakeFiles/game.dir/src/gameCore/Chess_game.cpp.o"
+  "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/src/gameCore/pieces/King.cpp" "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/build/CMakeFiles/game.dir/src/gameCore/pieces/King.cpp.o"
+  "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/src/gameCore/pieces/Knight.cpp" "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/build/CMakeFiles/game.dir/src/gameCore/pieces/Knight.cpp.o"
+  "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/src/gameCore/pieces/Piece.cpp" "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/build/CMakeFiles/game.dir/src/gameCore/pieces/Piece.cpp.o"
+  "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/src/main.cpp" "/home/marian/Marian/programming/cpp/chess_cpp/chess_cpp/build/CMakeFiles/game.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/src/main.cpp.o"
+  "CMakeFiles/game.dir/src/gameCore/Board.cpp.o"
+  "CMakeFiles/game.dir/src/gameCore/Chess_game.cpp.o"
+  "CMakeFiles/game.dir/src/gameCore/pieces/King.cpp.o"
+  "CMakeFiles/game.dir/src/gameCore/pieces/Knight.cpp.o"
+  "CMakeFiles/game.dir/src/gameCore/pieces/Piece.cpp.o"
+  "game.pdb"
+  "game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
